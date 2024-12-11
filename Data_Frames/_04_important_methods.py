87acb48cc3597478c2ba21a824eac300bf3e7412
Ashis-Mishra07 <mishralucky074@gmail.com>
2024-12-11 12:12:20 +0530

    partially commit

---
+++ Data_Frames/_04_important_methods.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+
+
+# Adding new cols
+# if this column is not present it will write a new column 
+movies['Country'] = 'India'
+
+
+# from existing ones
+# Making a new column lead actor from the actor column
+movies['lead actor'] = movies['actors'].str.split('|').apply(lambda x:x[0])
+
+
+
+
+
+# Important DataFrame Methods
+
+
+# 1. astype() - to change the datatype of a column
+ipl['ID'] = ipl['ID'].astype('int32')
+
+ipl['Season'] = ipl['Season'].astype('category')
+
+
+
